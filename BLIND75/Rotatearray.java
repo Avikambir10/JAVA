31ede4ad5448df7559435825ffdf841368fab053
Avikam <avikam198.be22@chitkara.edu.in>
2024-08-23 16:11:10 +0530

    "prime"

--- BLIND75/Rotatearray.java
+++ BLIND75/Rotatearray.java
@@ -1,23 +1,27 @@
 package BLIND75;
 
 import java.util.Scanner;
+
 class Solution {
-    public void rotate(int[] arr ,int k){
-        k = k% arr.length;
-        reverse(arr,0,arr.length-1);
-        reverse(arr,0,k-1);
-        reverse(arr,k,arr.length-1);
+    public void rotate(int[] arr, int k) {
+        k = k % arr.length;
+        reverse(arr, 0, arr.length - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, arr.length - 1);
     }
-    private void reverse(int[] arr,int l,int r){
-        while(l<r) {
+
+    private void reverse(int[] arr, int l, int r) {
+        while (l < r) {
             swap(arr, l++, r--);
         }
     }
-    private void  swap(int [] arr,int l,int r){
+
+    private void swap(int[] arr, int l, int r) {
         int temp = arr[l];
         arr[l] = arr[r];
         arr[r] = temp;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -29,9 +33,9 @@ class Solution {
         int k = sc.nextInt();
 
         Solution obj = new Solution();
-        obj.rotate(arr,k);
-        for(int nums : arr){
-            System.out.print( nums + " ");
+        obj.rotate(arr, k);
+        for (int nums : arr) {
+            System.out.print(nums + " ");
         }
         sc.close();
     }
