ee1f01f961ab911ee3c9974ba153887fcd0b9934
Avikam <avikam198.be22@chitkara.edu.in>
2024-09-13 15:26:47 +0530

    "new"

--- BLIND75/product_of_array_except_self.java
+++ BLIND75/product_of_array_except_self.java
@@ -10,33 +10,33 @@ import java.util.Scanner;
 
 // Input: nums = [-1,1,0,-3,3]
 // Output: [0,0,9,0,0]
- 
+
 // https://leetcode.com/problems/product-of-array-except-self/?envType=study-plan-v2&envId=leetcode-75
 public class product_of_array_except_self {
-    public static int[] product_of_array(int[] arr){
+    public static int[] product_of_array(int[] arr) {
         int n = arr.length;
-        
+
         int[] prefix = new int[n];
         int[] suffix = new int[n];
-        
+
         prefix[0] = 1;
         for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i-1]*arr[i-1];
-        } 
-        suffix[suffix.length-1] = 1;
-        for (int i = n-2; i >= 0; i--) {
-            suffix[i] = suffix[i+1] * arr[i+1];
+            prefix[i] = prefix[i - 1] * arr[i - 1];
         }
-        
-        int [] ans = new int[n];
+        suffix[suffix.length - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * arr[i + 1];
+        }
+
+        int[] ans = new int[n];
         for (int i = 0; i < ans.length; i++) {
-            ans[i] = prefix[i] *suffix[i];
+            ans[i] = prefix[i] * suffix[i];
         }
         return ans;
     }
 
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
---
+++ ClassWork/AddBinary.java
@@ -0,0 +1,35 @@
+package ClassWork;
+
+import java.util.Scanner;
+
+public class AddBinary {
+    public static String ans(String a, String b) {
+        StringBuilder res = new StringBuilder();
+        int carry = 0;
+        int i = a.length()-1;
+        int j = b.length()-1;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                sum += b.charAt(j--) - '0';
+            }
+            carry = (sum > 1) ? 1 : 0;
+            res.append(sum%2);
+
+        }
+        if (carry != 0) {
+            res.append(carry);
+        }
+        return res.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        System.out.println(ans(a, b));
+    }
+}
