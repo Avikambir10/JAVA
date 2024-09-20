a0257719d2fc32d9ceb2a3e8b6857e61a7d0d456
Avikam <avikam198.be22@chitkara.edu.in>
2024-09-20 16:13:11 +0530

    "new"

--- ClassWork/AddBinary.java
+++ ClassWork/AddBinary.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class AddBinary {
     public static String ans(String a, String b) {
         StringBuilder res = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
         int carry = 0;
-        int i = a.length()-1;
-        int j = b.length()-1;
         while (i >= 0 || j >= 0) {
             int sum = carry;
             if (i >= 0) {
@@ -16,11 +16,10 @@ public class AddBinary {
             if (j >= 0) {
                 sum += b.charAt(j--) - '0';
             }
-            carry = (sum > 1) ? 1 : 0;
-            res.append(sum%2);
-
+            carry = sum > 1 ? 1 : 0;
+            res.append(sum % 2);
         }
-        if (carry != 0) {
+        if(carry!= 0){
             res.append(carry);
         }
         return res.reverse().toString();
---
+++ St_2/StringPalindrome.java
@@ -0,0 +1,50 @@
+package St_2;
+//Find occurrences of palindrome words in a string
+//Write a function which given a string, count and return the palindrome words present in the string. A word in a string is separated with space(s).
+//
+//Sample Input 1
+//
+//Mom and Dad are my best friends
+//Sample Output 1
+//2
+//Explanation 1
+//This string contains two palindrome words (i.e., Mom, Dad) so the count is 2.
+//
+// Sample Input 2
+//
+//mohit speaks english
+//Sample Output 2
+//0
+//Explanation 2
+//This string contains no palindrome words
+public class StringPalindrome {
+    public static boolean isPali(String word){
+        int n = word.length();
+        int left = 0;
+        int right = n -1;
+        for (int i = 0; i < n; i++) {
+            if(Character.toLowerCase(word.charAt(left)) != Character.toLowerCase(word.charAt(right))){
+                return false;
+            }
+            left++;right--;
+        }
+
+        return true;
+    }
+    public static int countPali(String str){
+        String[] words = str.split("\\s+");
+
+        int cnt = 0;
+        for(String word:words){
+            if(isPali(word)){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        String str = "Mom and Dad are my best friends";
+        System.out.println(countPali(str));
+    }
+}
